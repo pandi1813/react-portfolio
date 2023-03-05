9a47a75b43ea4f11a79a4229cc811ea899483717
pandi1813 <andrea.peter1813@gmail.com>
2023-03-05 11:23:33 +0000

    imported project details json file to project gallery and set up a map function

--- src/components/pages/ProjectGallery.jsx
+++ src/components/pages/ProjectGallery.jsx
@@ -5,10 +5,14 @@ import ProjectCard from "../ProjectCard"
 
 
 export default function ProjectGallery() {
+    let projectDetails = require('../ProjectDescription.json')
+    console.log(projectDetails.map(projectDetail => projectDetail.name))
+    
     return (
         <>
         <h1>Projects</h1>
         <div className="row">
+
             <ProjectCard projectName="test" description="some description" tech1="react" tech2="react router" tech3="bootstrap" github="github repo" live="live page"/>
             <ProjectCard/>
             <ProjectCard/>
