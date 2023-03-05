3c42377a0aaa5ac9ea35c051532b9c2fffb3edd5
pandi1813 <andrea.peter1813@gmail.com>
2023-03-05 11:58:48 +0000

    used map function to dinamically display project cards

--- src/components/pages/ProjectGallery.jsx
+++ src/components/pages/ProjectGallery.jsx
@@ -12,20 +12,11 @@ export default function ProjectGallery() {
         <>
         <h1>Projects</h1>
         <div className="row">
-        
-            {/* <ProjectCard projectName={projectDetails[0].projectName} descriptionShort={projectDetails[0].descriptionShort} tech1={projectDetails[0].tech1} tech2={projectDetails[0].tech2} tech3={projectDetails[0].tech3} github={projectDetails[0].github} liveLink={projectDetails[0].liveLink}/> */}
-             <ProjectCard {...projectDetails[0]}/>
-             <ProjectCard {...projectDetails[1]}/>
-             <ProjectCard {...projectDetails[2]}/>
-             <ProjectCard/>
-             <ProjectCard/>
+            {projectDetails.map(projectDetail => <ProjectCard {...projectDetail}/>)}
+           
         </div>
 
-        {/* <Router>
-            <Routes>
-            <Route path="project" element={<Project />} />
-            </Routes>
-        </Router> */}
+    
         </>
     )
 }
