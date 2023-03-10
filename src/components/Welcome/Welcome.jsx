f7f7a19e2b46a80e27cdd677c0555856a116ff3f
pandi1813 <andrea.peter1813@gmail.com>
2023-03-10 21:57:06 +0000

    basic homepage styling

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -5,38 +5,12 @@ export default function Welcome() {
         <>
             <div className='welcome'>
 
-
                 <h1>Welcome to my portfolio</h1>
                 <h2> I'm <span className='accent'>Andrea Peter</span></h2>
+          
             </div>
 
 
-
-
-
-
-
-            {/* <div class="sticky-container">
-                <div class="sticky-outer">
-                    <div class="sticky">
-                        <svg width="0" height="0">
-                            <defs>
-                                <clipPath id="stickyClip" clipPathUnits="objectBoundingBox">
-                                    <path
-                                        d="M 0 0 Q 0 0.69, 0.03 0.96 0.03 0.96, 1 0.96 Q 0.96 0.69, 0.96 0 0.96 0, 0 0"
-                                        stroke-linejoin="round"
-                                        stroke-linecap="square"
-                                    />
-                                </clipPath>
-                            </defs>
-                        </svg>
-                        <div class="sticky-content">
-                            <h1>Hello, <br /> I'm <span className="font-accent"> Andrea Peter</span></h1>
-                            <h2>Welcome to my portfolio</h2>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
         </>
     )
 }
