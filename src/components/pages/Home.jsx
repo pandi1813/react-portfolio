389b8c13053bfb1533e397bdc890be945300c363
pandi1813 <andrea.peter1813@gmail.com>
2023-03-10 19:37:00 +0000

    added first sticky note as a link to project gallery page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Welcome from "../Welcome/Welcome";
 import BrandStatement from "../BrandStatement/BrandStatement";
+import StickyNote from "../StickyNoteLinks/StickyNote";
 
 export default function Home() {
     return <>
        <Welcome/>
        <BrandStatement/>
+       <StickyNote/>
     </>
 }
